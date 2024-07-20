a8f896d248c88c7c3515bd8e4c9fa05c99b98a00
Vennila <nilarajan@gmail.com>
2024-07-20 11:59:49 +0800

    updating main.tf for sns_topics

--- main.tf
+++ main.tf
@@ -1,9 +1,6 @@
- resource "aws_instance" "app_server" {
-   ami           = "ami-08d70e59c07c61a3a"
-   instance_type = "t2.micro"
-
-   tags = {
+resource "aws_sns_topic" "user_updates" {
+  tags = {
     Name = "ven-c6-updates-topic"
     Name = var.instance_name
-   }
- }
+  }
+}
